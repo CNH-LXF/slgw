1eef9ecba1f6563eab2aaaad2bf92a62c1836d37
LXF <1012722764@qq.com>
2017-08-01 13:22:01 +0800

    规范了代码
    删减部门，改变活动介绍方式

---
+++ 社联官网规范版/js/activity.js
@@ -0,0 +1,33 @@
+ // $(document).ready(function() {
+ //        $('#fullpage').fullpage({
+ //        	navigation:true,
+ //        	navigationColor:'#fff',
+ //        	anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+ //        	//滚动到某一屏后产生的动画效果
+ //        afterLoad: function(link,sl){
+ //            switch (sl){
+ //                case 1:
+ //                    move('#title').scale(2).end();
+ //                    move('.section1 p').set('margin-top','5%').end();
+ //                    break;
+                
+                
+ //                default :
+ //                    break;
+ //            }
+ //        },
+ //         //离开某一屏后恢复到初始效果
+ //        onLeave: function(link, sl){
+ //            switch (sl){
+ //                case 1:
+ //                    move('.section1 h1').scale(1).end();
+ //                    move('.section1 p').set('margin-top','800px').end();
+ //                    break;
+               
+ //                default :
+ //                    break;
+ //            }
+ //        },
+       
+ //    })
+ //  });
